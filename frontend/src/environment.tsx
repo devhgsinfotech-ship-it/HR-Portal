6953a1ca08df6a42c30c5789a260f5e85976435e
Vikramjeet <devhgsinfotech@gmail.com>
2026-08-14 05:09:33 +0000

    email verification pass fix production and local url

--- frontend/src/environment.tsx
+++ frontend/src/environment.tsx
@@ -6,10 +6,17 @@ export const img_path = '/'
 export const APP_CONFIG = {
   liveBackendUrl: 'https://api.aaups.com',
   localBackendUrl: 'http://localhost:5000',
+  liveFrontendDomain: 'aaups.com',
+  localFrontendDomain: 'localhost:3000',
   
   // Gets the correct backend URL based on whether you are running npm run dev or npm run build
   getBackendUrl: () => {
     // If VITE_API_URL is set in docker, use it. Otherwise use the automatic fallback.
     return import.meta.env.VITE_API_URL || (import.meta.env.DEV ? APP_CONFIG.localBackendUrl : APP_CONFIG.liveBackendUrl);
+  },
+  
+  // Gets the correct frontend domain based on whether you are running npm run dev or npm run build
+  getFrontendDomain: () => {
+    return import.meta.env.DEV ? APP_CONFIG.localFrontendDomain : APP_CONFIG.liveFrontendDomain;
   }
 };
